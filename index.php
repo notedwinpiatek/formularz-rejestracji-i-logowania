363f7b036b074129532bd52fd0959319b875d7f2
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-05-12 21:39:10 +0200

    form

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+?>
+<form action="index.php" method="get">
+    <label for="emailInput">Email:</label>
+    <input type="email" name="email" id="emailInput" required>
+    <label for="passwordInput">Password:</label>
+    <input type="password" name="password" id="passwordInput" required>
+</form>
