7cb5a86e46ded4809981786ad9efb6858467eb32
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-05-12 22:57:50 +0200

    autoryzacja użytkownika

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
 <?php
+if(isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
 
-$email = $_REQUEST['email'];
-$password = $_REQUEST['password'];
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL); 
 
-$email = filter_var($email, FILTER_SANITIZE_EMAIL); 
+    $db = new mysqli("localhost", "root", "", "auth");
+    $q = "SELECT * FROM user WHERE email = $email";
+    $db->query($q);
 
-$db = new mysqli("localhost", "root", "", "auth");
-$q = "SELECT * FROM user WHERE email = $email";
-$db->query($q);
+    $q = $db->prepare("SELECT * FROM user WHERE email = ? LIMIT 1");
 
-$q = $db->prepare("SELECT * FROM user WHERE email = ?");
+    $q->bind_param("s", $email);
+    $q->execute();
+    $result = $q->get_result();
 
-$q->bind_param("s", $email);
-$q->execute();
-$result = $q->get_result();
+    $userRow = $result->fetch_assoc();
+    if($userRow == null) {  
+        echo "Błędny login lub hasło <br>";
+    } else {
+        if(password_verify($password, $userRow['passwordHash'])) {
+            echo "Zalogowano pomyślnie <br>";
+        } else {
+            echo "Błędny login lub hasło <br>";
+        }
+    } 
+}
+
 
 
 ?>
