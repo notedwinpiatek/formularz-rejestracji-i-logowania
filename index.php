cd20b29a4247f34751e32ade26f3841ad3044ec8
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-05-12 21:40:40 +0200

    submit

--- index.php
+++ index.php
@@ -8,4 +8,5 @@
     <input type="email" name="email" id="emailInput" required>
     <label for="passwordInput">Password:</label>
     <input type="password" name="password" id="passwordInput" required>
+    <input type="submit" value="Zaloguj">
 </form>
